5d1276fa08dae76e58f12b76cf5b53d3482b046a
yoonjuho <dbswn990@naver.com>
2023-01-08 17:23:59 +0900

    feat: JDBC Template

---
+++ introduction/src/main/java/com/example/introduction/service/SpringConfig.java
@@ -0,0 +1,35 @@
+package com.example.introduction.service;
+
+import com.example.introduction.repository.JdbcTemplateMemberRepository;
+import com.example.introduction.repository.JpaMemberRepository;
+import com.example.introduction.repository.MemberRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+//    private final DataSource dataSource;
+    @PersistenceContext
+    private EntityManager em;
+
+    public SpringConfig(EntityManager em){
+        this.em = em;
+    }
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+    @Bean
+    public MemberRepository memberRepository() {
+        //return new MemoryMemberRepository();
+        //return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
+    }
+
+}
---
+++ introduction/src/test/java/com/example/introduction/service/MemberServiceTest.java
@@ -0,0 +1,57 @@
+package com.example.introduction.service;
+
+import com.example.introduction.domain.Member;
+
+import com.example.introduction.repository.MemberRepository;
+import com.example.introduction.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+//    @BeforeEach
+//    public void beforeEach() {
+//        memberRepository = new MemoryMemberRepository();
+//        memberService = new MemberService(memberRepository);
+//    }
+//    @AfterEach
+//    public void afterEach() {
+//        memberRepository.clearStore();
+//    }
+    @Test
+//    @Commit
+    public void 회원가입() throws Exception {
+//Given
+        Member member = new Member();
+        member.setName("hello");
+//When
+        Long saveId = memberService.join(member);
+//Then
+        Member findMember = memberRepository.findById(saveId).get();
+        assertEquals(member.getName(), findMember.getName());
+    }
+    @Test
+    public void 중복_회원_예외() throws Exception {
+//Given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        Member member2 = new Member();
+        member2.setName("spring1");
+        //When
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));//예외가 발생해야 한다.
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+    } }
