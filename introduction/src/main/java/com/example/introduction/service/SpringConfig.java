e957e8624ddfbc2685695b1e577c202e5edb41f5
yoonjuho <dbswn990@naver.com>
2023-01-08 18:19:38 +0900

    feat: AOP

--- introduction/src/main/java/com/example/introduction/service/SpringConfig.java
+++ introduction/src/main/java/com/example/introduction/service/SpringConfig.java
@@ -1,5 +1,6 @@
 package com.example.introduction.service;
 
+import com.example.introduction.aop.TimeTraceAop;
 import com.example.introduction.repository.JdbcTemplateMemberRepository;
 import com.example.introduction.repository.JpaMemberRepository;
 import com.example.introduction.repository.MemberRepository;
@@ -32,6 +33,11 @@ public class SpringConfig {
 //        //return new JdbcMemberRepository(dataSource);
 ////        return new JdbcTemplateMemberRepository(dataSource);
 //        return new JpaMemberRepository(em);
+//    }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop(){
+//        return new TimeTraceAop();
 //    }
 
 }
