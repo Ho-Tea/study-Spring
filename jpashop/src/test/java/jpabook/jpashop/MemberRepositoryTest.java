63075aa12fa499d08cf69bde989ed19d559bc7d5
yoonjuho <dbswn990@naver.com>
2023-01-31 20:18:40 +0900

    feat: 활용1(도메인 분석 설계)

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -17,19 +17,19 @@ class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
 
-    @Test
-    @Transactional
-    public void testMember() throws Exception{
-        Member member = new Member();
-        member.setUsername("memberA");
-        Long saveId = memberRepository.save(member);
-
-        Member findMember = memberRepository.find(saveId);
-
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-
-        Assertions.assertThat(findMember).isEqualTo(member);
-
-    }
+//    @Test
+//    @Transactional
+//    public void testMember() throws Exception{
+//        Member member = new Member();
+//        member.setUsername("memberA");
+//        Long saveId = memberRepository.save(member);
+//
+//        Member findMember = memberRepository.find(saveId);
+//
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//
+//        Assertions.assertThat(findMember).isEqualTo(member);
+//
+//    }
 }
