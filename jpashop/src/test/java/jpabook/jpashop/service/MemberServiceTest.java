b8d5803cca377fa183e69c873febd7d39288ef88
yoonjuho <dbswn990@naver.com>
2023-02-01 21:12:10 +0900

    feat: 활용1(회원 도메인 개발)

---
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,57 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception{
+
+        //Given
+        Member member = new Member();
+        member.setName("kim1");
+
+        //When
+        Long saveId = memberService.join(member);
+
+        //Then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception{
+        Member member1 = new Member();
+        member1.setName("kim2");
+        Member member2 = new Member();
+        member2.setName("kim2");
+
+        try{
+            memberService.join(member1);
+            memberService.join(member2);}
+        catch (IllegalStateException e){
+            return;
+        }
+
+        fail("여기로 오면 안된다");
+    }
+
+}
