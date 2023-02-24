95a4c803791f5b509427ef5a6efecb06cb6efcfa
yoonjuho <dbswn990@naver.com>
2023-02-25 02:29:42 +0900

    feat: JPA활용2 프로젝트 설치

---
+++ JPA/jpa/src/main/java/com/example/jpa/JpamasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpamasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpamasterApplication.class, args);
+	}
+
+}
---
+++ JPA/jpa/src/main/java/com/example/jpa/api/MemberApiController.java
@@ -0,0 +1,42 @@
+package com.example.jpa.api;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member meber){
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+        Member member = new Member();
+        member.setName(request.getName());
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+    @Data
+    static class CreateMemberRequest {
+        @NotEmpty
+        private String name;
+    }
+    @Data
+    static class CreateMemberResponse{
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
