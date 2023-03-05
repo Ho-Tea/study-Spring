22ba797555b5318981ed818d6a8ad1ebcd202639
yoonjuho <dbswn990@naver.com>
2023-03-05 19:30:12 +0900

    feat: OrderSearch domain

---
+++ JPA/jpa/src/main/java/com/example/jpa/domain/OrderSearch.java
@@ -0,0 +1,11 @@
+package com.example.jpa.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+    private String memberName;
+    private OrderStatus orderStatus;
+}
