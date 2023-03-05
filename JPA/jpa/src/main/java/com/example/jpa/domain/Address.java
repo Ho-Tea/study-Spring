2072b03e3abff3b5ba4d12bab22ff2adc7ba81c6
yoonjuho <dbswn990@naver.com>
2023-03-05 18:35:03 +0900

    refactor: Address domain

--- JPA/jpa/src/main/java/com/example/jpa/domain/Address.java
+++ JPA/jpa/src/main/java/com/example/jpa/domain/Address.java
@@ -1,10 +1,22 @@
 package com.example.jpa.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.Getter;
 
 @Embeddable
+@Getter
 public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    public Address(){
+
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
