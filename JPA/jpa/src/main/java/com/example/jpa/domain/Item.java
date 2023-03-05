3d151eaea8f7d0b81a1692ae28fa8a17a5753b2d
yoonjuho <dbswn990@naver.com>
2023-03-05 23:56:00 +0900

    refactor: Item domain

--- JPA/jpa/src/main/java/com/example/jpa/domain/Item.java
+++ JPA/jpa/src/main/java/com/example/jpa/domain/Item.java
@@ -1,5 +1,6 @@
 package com.example.jpa.domain;
 
+import com.example.jpa.exception.NotEnoughStockException;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -24,5 +25,16 @@ public class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
 
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
 
+    }
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+
+        }
+        this.stockQuantity = restStock;
+    }
 }
