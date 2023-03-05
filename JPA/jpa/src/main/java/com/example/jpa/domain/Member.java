d906670db104a52350e07b0a2f10cf667c9be602
yoonjuho <dbswn990@naver.com>
2023-03-05 18:34:31 +0900

    refactor: Member domain

--- JPA/jpa/src/main/java/com/example/jpa/domain/Member.java
+++ JPA/jpa/src/main/java/com/example/jpa/domain/Member.java
@@ -2,10 +2,14 @@ package com.example.jpa.domain;
 
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Getter @Setter
 public class Member {
     @Id @GeneratedValue
     private Long id;
@@ -15,8 +19,8 @@ public class Member {
     @Embedded
     private Address address;
 
-    @OneToMany(mappedBy = member, fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
     private List<Order> orders = new ArrayList<>();
-    
+
 
 }
