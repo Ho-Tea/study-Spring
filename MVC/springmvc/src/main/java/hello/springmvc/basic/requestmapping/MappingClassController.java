1c7866f0e678cfe71bc57d911dd7d6ea5f02e04c
yoonjuho <dbswn990@naver.com>
2023-04-10 18:50:23 +0900

    스프링MVC(기본기능)

---
+++ MVC/springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -0,0 +1,12 @@
+package hello.springmvc.basic.requestmapping;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RestController
+public class MappingClassController {
+}
